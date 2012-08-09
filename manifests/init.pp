f1e94cbb8659d45a27f86603d1aa52697b9ab4e4
KURASHIKI Satoru <lurdan@gmail.com>
2012-08-09 17:15:29 +0900

    fix resource name to change module name.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
-# Class: request-tracker
+# Class: requesttracker
 #
-class request-tracker ( $version = 'present', $major = false, $db = 'mysql' ) {
+class requesttracker ( $version = 'present', $major = false, $db = 'mysql' ) {
 
   if $major {
-    $_major = $major
+    $rtmajor = $major
   } else {
-    $_major = $::lsbdistcodename ? {
+    $rtmajor = $::lsbdistcodename ? {
       'lenny' => '3.6',
       'squeeze' => '3.8',
       'wheezy' => '4',
@@ -13,19 +13,19 @@ class request-tracker ( $version = 'present', $major = false, $db = 'mysql' ) {
   }
 
   package {
-    'request-tracker':
-      name => "request-tracker${_major}",
+    'requesttracker':
+      name => "request-tracker${rtmajor}",
       ensure => $version,
-      responsefile => defined(Apt::Preseed['request-tracker']) ? {
-        true => '/var/cache/debconf/request-tracker.preseed',
+      responsefile => defined(Apt::Preseed['requesttracker']) ? {
+        true => '/var/cache/debconf/requesttracker.preseed',
         default => false,
       };
     "rt-db-${db}":
-      name => "rt${_major}-db-${db}",
-      before => Package['request-tracker'];
+      name => "rt${rtmajor}-db-${db}",
+      before => Package['requesttracker'];
   }
 #  'libdatetime-perl':
-#    before => Package['request-tracker'];
+#    before => Package['requesttracker'];
 
   exec { 'update-rt-siteconfig':
     command => '/usr/sbin/update-rt-siteconfig',
@@ -33,39 +33,39 @@ class request-tracker ( $version = 'present', $major = false, $db = 'mysql' ) {
   }
 }
 
-# Definition: request-tracker::plugin
+# Definition: requesttracker::plugin
 #
 # Usage:
-#  request-tracker::plugin {
+#  requesttracker::plugin {
 #    'calendar':
 #      config => template('rt-siteconfig-calendar.erb');
 #    'jsgantt':
 #      config => template('rt-siteconfig-jsgantt.erb');
 # }
 
-define request-tracker::plugin ( $config ) {
-  realize(Request-tracker::Siteconfig['Plugins'])
+define requesttracker::plugin ( $config ) {
+  realize(Requesttracker::Siteconfig['Plugins'])
 
   $package_name = $name ? {
-    'calendar' => "rt${request-tracker::_major}-extension-calendar",
-    'jsgantt' => "rt${request-tracker::_major}-extension-jsgantt",
-	'gravatar' => "rt${request-tracker::_major}-extension-gravatar",
-    #'' => "rt${request-tracker::_major}-",
+    'calendar' => "rt${requesttracker::rtmajor}-extension-calendar",
+    'jsgantt' => "rt${requesttracker::rtmajor}-extension-jsgantt",
+	'gravatar' => "rt${requesttracker::rtmajor}-extension-gravatar",
+    #'' => "rt${requesttracker::rtmajor}-",
   }
 
   package { "$package_name":
-    require => Package['request-tracker'],
-#    before => Service['request-tracker'],
+    require => Package['requesttracker'],
+#    before => Service['requesttracker'],
   }
 
-  request-tracker::siteconfig { "$name":
+  requesttracker::siteconfig { "$name":
     content => $config,
   }
 }
 
-# Definition: request-tracker::siteconfig
+# Definition: requesttracker::siteconfig
 #
-# set config file to /etc/request-tracker/RT_SiteConfig.d/.
+# set config file to /etc/requesttracker/RT_SiteConfig.d/.
 #
 # Parameters:
 #   $content: required, pass through to file resource.
@@ -75,13 +75,13 @@ define request-tracker::plugin ( $config ) {
 # Requires:
 #
 # Sample Usage:
-#   request-tracker::siteconfig { "jafont":
+#   requesttracker::siteconfig { "jafont":
 #     content => "Set(%ChartFont, 'ja' => '/usr/share/fonts/truetype/ttf-japanese-gothic.ttf');",
 #   }
-define request-tracker::siteconfig ( $content, $order = '40' ) {
-  file { "/etc/request-tracker${request-tracker::_major}/RT_SiteConfig.d/${order}-${name}":
+define requesttracker::siteconfig ( $content, $order = '40' ) {
+  file { "/etc/request-tracker${requesttracker::rtmajor}/RT_SiteConfig.d/${order}-${name}":
     content => $content,
-    require => Package['request-tracker'],
+    require => Package['requesttracker'],
     notify => Exec['update-rt-siteconfig'],
   }
 }
